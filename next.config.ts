90ef5e30f60025da245c60f7d98f78ce4ab004bd
weishu <weishuchine@163.com>
2026-02-28 17:35:30 +0800

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+ import type { NextConfig } from "next";
+
+  const nextConfig: NextConfig = {
+    experimental: {
+      serverActions: {
+        bodySizeLimit: '10mb',
+      },
+    },
+  };
+
+  export default nextConfig;
+
+  ---
